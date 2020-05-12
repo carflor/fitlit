f2f711c71ef15831d51cf9355cc87469db7f81ab
carflor <cfdeclet@gmail.com>
2020-05-11 20:26:51 -0600

    Rename data to allUsers for improved naming convention

--- src/UserRepo.js
+++ src/UserRepo.js
@@ -1,21 +1,21 @@
 const userData = require('../data/users.js')
 
 class UserRepo {
-  constructor(data) {
-    this.data = data;
+  constructor(allUsers) {
+    this.data = allUsers;
   }
 
-  selectedUser() {
-    return this.data.filter(datum => datum.id === currentUserId)
+  selectedUser(currentUserId) {
+    // currentUserId needs to be built in dom
+    return this.allUsers.filter(user => user.id === currentUserId)
   }
 
   avgStepGoalUsers() {
-    return this.data.reduce((acc, datum) => {
-      (acc += datum.dailyStepGoal)/this.data.length
+    return this.allUsers.reduce((acc, user) => {
+      (acc += user.dailyStepGoal) / this.allUsers.length
       return acc
     }, 0)
   }
-
 };
 
 module.exports = UserRepo;
