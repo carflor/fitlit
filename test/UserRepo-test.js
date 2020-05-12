3ac375747349586e7b03c8164a18553aceeabcb8
carflor <cfdeclet@gmail.com>
2020-05-12 14:59:46 -0600

    Refactor to use test data file

--- test/UserRepo-test.js
+++ test/UserRepo-test.js
@@ -2,40 +2,14 @@ const chai = require('chai');
 const expect = chai.expect;
 const UserRepo = require('../src/UserRepo');
 const User = require('../src/User');
-// const testData = require('../test/test-data.js');
+const userTestData = require('../test/test-data');
 
 describe('UserRepo', function() {
   let user1, user2, userRepo, allUsers;
 
   beforeEach(function() {
-    // allUsers = new UserRepo(userTestData);
-    user1 = new User({
-      "id": 1,
-      "name": "Luisa Hane",
-      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-      "email": "Diana.Hayes1@hotmail.com",
-      "strideLength": 4.3,
-      "dailyStepGoal": 10000,
-      "friends": [
-        2,
-        4,
-        3
-      ]
-    })
-    user2 = new User({
-      "id": 2,
-      "name": "Jarvis Considine",
-      "address": "30086 Kathryn Port, Ciceroland NE 07273",
-      "email": "Dimitri.Bechtelar11@gmail.com",
-      "strideLength": 4.5,
-      "dailyStepGoal": 5000,
-      "friends": [
-        9,
-        18,
-        24,
-        19
-      ]
-    })
+    user1 = userTestData[0]
+    user2 = userTestData[1]
     allUsers = [user1, user2]
     userRepo = new UserRepo(allUsers)
   })
