98712e82649e8901271e166bd0344259698487b3
carflor <cfdeclet@gmail.com>
2020-05-13 14:27:34 -0600

    Add fn for displaying data of all users

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let currentUser;
 let userRepo;
 
@@ -10,15 +12,13 @@ function startApp() {
   console.log(currentUser)
   displayUserData(currentUser)
 
-  // grab number from randomizer user
-  // instantiate userRepo
   // instantiate the user
   // instantiate sleep / hydration / activity
   // display all data
 }
 
 function pickUser() {
-  const id = userData[0]
+  const id = userData[15]
   // fixed randomizer
   // const id = userData[Math.floor(Math.random() * 50)]
   return id
@@ -26,17 +26,19 @@ function pickUser() {
 
 function displayUserData(user) {
   const userName = document.querySelector('.user-name')
-  const userDate = document.querySelector('.date')
+  const userDate = document.querySelector('.title-date')
   const userAddress = document.querySelector('.user-address')
   const userEmail = document.querySelector('.user-email')
   const userStepGoal = document.querySelector('.user-step-goals')
+  const allUsersStepGoals = document.querySelector('.all-users-step-goals')
 
   userName.innerText = user.name
   // create fn to calculate curr depending on data
-  userDate.innerText = '06/15/2019'
+  userDate.innerText = `Step Goal for Today: 06/15/2019`
   userAddress.innerText = user.address
   userEmail.innerText = user.email
-  userStepGoal.innerText = user.dailyStepGoal
+  userStepGoal.innerText = `${user.dailyStepGoal} Steps`
+  allUsersStepGoals.innerText = `${userRepo.calculateAvgStepGoalUsers()} Steps`
 }
 
 startApp()
