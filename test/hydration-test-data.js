87b66ea56d7b8f58f8a9f44527258027cdc70e19
carflor <cfdeclet@gmail.com>
2020-05-14 16:07:44 -0600

    Add activitiy data to test file

---
+++ test/hydration-test-data.js
@@ -0,0 +1,107 @@
+const hydrationTestData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numOunces": 37
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 75
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numOunces": 47
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/15",
+    "numOunces": 85
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numOunces": 69
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "numOunces": 91
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "numOunces": 99
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/16",
+    "numOunces": 95
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "numOunces": 96
+  }, {
+    "userID": 1,
+    "date": "2019/06/18",
+    "numOunces": 61
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/19",
+    "numOunces": 91
+  }, 
+  {
+    "userID": 1,
+    "date": "2019/06/20",
+    "numOunces": 50
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/21",
+    "numOunces": 50
+  }, 
+  {
+    "userID": 1,
+    "date": "2019/06/22",
+    "numOunces": 43
+  }, 
+  {
+    "userID": 1,
+    "date": "2019/06/23",
+    "numOunces": 39
+  }, 
+  {
+    "userID": 1,
+    "date": "2019/06/24",
+    "numOunces": 61
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/25",
+    "numOunces": 51
+  }, 
+  {
+    "userID": 1,
+    "date": "2019/06/26",
+    "numOunces": 52
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/27",
+    "numOunces": 29
+  }, 
+  {
+    "userID": 1,
+    "date": "2019/06/28",
+    "numOunces": 57
+  }, 
+  {
+    "userID": 1,
+    "date": "2019/06/29",
+    "numOunces": 99
+  }];
+
+module.exports = hydrationTestData;
