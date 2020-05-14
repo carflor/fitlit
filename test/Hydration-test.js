f3366e390876217c643790a1fbc1c5009b38d562
carflor <cfdeclet@gmail.com>
2020-05-14 16:17:04 -0600

    Fix test for hydration class

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -1,35 +1,44 @@
 const chai = require('chai');
 const expect = chai.expect;
 const User = require('../src/User');
-const userTestData = require('../test/test-data');
-const hydrationTestData = require('../test/test-data')
+const userTestData = require('../test/user-test-data');
+const hydrationTestData = require('../test/hydration-test-data')
 const Hydration = require('../src/Hydration')
 
 describe('Hydration', function () {
-  let hydration, today;  
+  let hydration, today, hydrationData, user, user1;  
+
   beforeEach(function() {
-    today = "2019/06/29";
+    today = '2019/06/29';
     hydrationData = hydrationTestData;
     hydration = new Hydration(hydrationData)
+    user1 = userTestData[0]
+    user = new User(user1)
   })
+
   it('should be a function', function() {
     expect(Hydration).to.be.a('function')
   })
+
   it('should be an instance of Hydration', function() {
     expect(hydration).to.be.an.instanceOf(Hydration)
   })
+
   it('should have a property of data', function() {
     expect(hydration.hydrationData).to.equal(hydrationData)
   })
+
   it('should be able to get the avg ounces for a user today', function() {
-    expect(hydration.getUserAvgOuncesToday(hydrationTestData, 1, today)).to.equal(37)
+    expect(hydration.getUserAvgOuncesToday(hydrationTestData, user, today)).to.equal(99)
   })
+
   it('should be able to get the avg ounces for a user all time', function() {
-    expect(hydration.getUserAvgOuncesAllTime(hydrationTestData, 1)).to.equal(59)
+    expect(hydration.getUserAvgOuncesAllTime(hydrationTestData, user)).to.equal(59)
   })
+
   it('should give avg ounces per day for a week', function() {
-    expect(hydration.getUserWeekHydration(hydrationData, 1, today).length).to.equal(7)
+    expect(hydration.getUserWeekHydration(hydrationData, user, today).length).to.equal(7)
   })
-})
+});
 
 
