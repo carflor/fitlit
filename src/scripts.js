13dd4501a3d369c251015602931f020e75ce443f
carflor <cfdeclet@gmail.com>
2020-05-14 15:45:40 -0600

    Changed for loop indent

--- src/scripts.js
+++ src/scripts.js
@@ -4,19 +4,12 @@ let currentUser;
 let userRepo;
 let hydrationRepo;
 
-console.log('user data', userData);
-console.log('hydro data', hydrationData)
-
 function startApp() {
   const today = '2019/06/29'
   const currentUser = new User(pickUser())
   userRepo = new UserRepo(userData)
-  // currentUser = new User(currentUserId)
   hydrationRepo = new Hydration(hydrationData)
-  console.log('currentUser', currentUser)
-  console.log('currentUserId', currentUser)
   displayUserData(currentUser, today)
-
   displayUserHydrationData(hydrationData, currentUser, today)
 }
 
@@ -36,7 +29,6 @@ function displayUserData(user, date) {
   const allUsersStepGoals = document.querySelector('.all-users-step-goals')
 
   userName.innerText = user.name
-  // create fn to calculate curr depending on data
   userDate.innerText = `Step Goal for Today: ${date}`
   userAddress.innerText = user.address
   userEmail.innerText = user.email
@@ -47,11 +39,10 @@ function displayUserData(user, date) {
 function displayUserHydrationData(data, user, date) {
   const userHydrationToday = document.querySelector('.user-hydration-today')
   const userHydrationWeek = document.querySelector('.user-hydration-week')
-  console.log('user in fn', user)
+
   userHydrationToday.innerText = `Ounces drank today: ${hydrationRepo.getUserAvgOuncesToday(data, user, date)}`
   console.log(hydrationRepo.getUserWeekHydration(data, user, date))
   userHydrationWeek.insertAdjacentHTML('afterBegin', `Ounces drank this week: ${hydrationRepo.getUserWeekHydration(data, user, date)}`)
-
 }
 
 startApp()
