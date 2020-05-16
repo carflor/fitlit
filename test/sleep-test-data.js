a8f17204d6722422a0c3509fd558d80962cb0095
Corbin March <marchcorbin@gmail.com>
2020-05-15 19:07:10 -0600

    Testing written for the sleep class

--- test/sleep-test-data.js
+++ test/sleep-test-data.js
@@ -1,45 +1,140 @@
 const sleepTestData = [
   {
     "userID": 1,
-    "date": "2019/06/15",
-    "hoursSlept": 6.1,
+    "date": "2019/09/14",
+    "hoursSlept": 6.5,
+    "sleepQuality": 4.2
+  }, {
+    "userID": 2,
+    "date": "2019/09/14",
+    "hoursSlept": 8.7,
+    "sleepQuality": 1.6
+  }, {
+    "userID": 3,
+    "date": "2019/09/14",
+    "hoursSlept": 4.8,
     "sleepQuality": 2.2
-  },
-  {
-    "userID": 2,
-    "date": "2019/06/15",
-    "hoursSlept": 7,
-    "sleepQuality": 4.7
-  },
-  {
-    "userID": 3,
-    "date": "2019/06/15",
-    "hoursSlept": 10.8,
-    "sleepQuality": 4.7
-  },
-  {
-    "userID": 4,
-    "date": "2019/06/15",
-    "hoursSlept": 5.4,
-    "sleepQuality": 3
-  },
-  {
+  }, {
     "userID": 1,
-    "date": "2019/06/16",
-    "hoursSlept": 4.1,
-    "sleepQuality": 3.8
-  },
-  {
+    "date": "2019/09/15",
+    "hoursSlept": 6.8,
+    "sleepQuality": 2
+  }, {
     "userID": 2,
-    "date": "2019/06/16",
-    "hoursSlept": 7.5,
-    "sleepQuality": 3.8
-  },
-  {
-    "userID": 3,
-    "date": "2019/06/16",
+    "date": "2019/09/15",
     "hoursSlept": 10.7,
-    "sleepQuality": 3.4
-  }];
+    "sleepQuality": 3.3
+  }, {
+    "userID": 3,
+    "date": "2019/09/15",
+    "hoursSlept": 8.2,
+    "sleepQuality": 1.5
+  }, {
+    "userID": 1,
+    "date": "2019/09/16",
+    "hoursSlept": 8.8,
+    "sleepQuality": 1.8
+  }, {
+    "userID": 2,
+    "date": "2019/09/16",
+    "hoursSlept": 7.4,
+    "sleepQuality": 2.7
+  }, {
+    "userID": 3,
+    "date": "2019/09/16",
+    "hoursSlept": 9.9,
+    "sleepQuality": 4.6
+  }, {
+    "userID": 1,
+    "date": "2019/09/17",
+    "hoursSlept": 6.3,
+    "sleepQuality": 3.2
+  }, {
+    "userID": 2,
+    "date": "2019/09/17",
+    "hoursSlept": 9.5,
+    "sleepQuality": 1.7
+  }, {
+    "userID": 3,
+    "date": "2019/09/17",
+    "hoursSlept": 8.4,
+    "sleepQuality": 1.8
+  }, {
+    "userID": 1,
+    "date": "2019/09/18",
+    "hoursSlept": 4.1,
+    "sleepQuality": 2.2
+  }, {
+    "userID": 2,
+    "date": "2019/09/18",
+    "hoursSlept": 4.8,
+    "sleepQuality": 3.8
+  }, {
+    "userID": 3,
+    "date": "2019/09/18",
+    "hoursSlept": 4.7,
+    "sleepQuality": 3.8
+  }, {
+    "userID": 1,
+    "date": "2019/09/19",
+    "hoursSlept": 9.9,
+    "sleepQuality": 1.7
+  }, {
+    "userID": 2,
+    "date": "2019/09/19",
+    "hoursSlept": 10.6,
+    "sleepQuality": 1.6
+  }, {
+    "userID": 3,
+    "date": "2019/09/19",
+    "hoursSlept": 7.4,
+    "sleepQuality": 1.7
+  }, {
+    "userID": 1,
+    "date": "2019/09/20",
+    "hoursSlept": 8.6,
+    "sleepQuality": 2.6
+  }, {
+    "userID": 2,
+    "date": "2019/09/20",
+    "hoursSlept": 8.5,
+    "sleepQuality": 4.3
+  }, {
+    "userID": 3,
+    "date": "2019/09/20",
+    "hoursSlept": 4.5,
+    "sleepQuality": 1.7
+  }, {
+    "userID": 1,
+    "date": "2019/09/21",
+    "hoursSlept": 10.7,
+    "sleepQuality": 4
+  }, {
+    "userID": 2,
+    "date": "2019/09/21",
+    "hoursSlept": 8.2,
+    "sleepQuality": 1.9
+  }, {
+    "userID": 3,
+    "date": "2019/09/21",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.5
+  }, {
+    "userID": 1,
+    "date": "2019/09/22",
+    "hoursSlept": 4.6,
+    "sleepQuality": 1.4
+  }, {
+    "userID": 2,
+    "date": "2019/09/22",
+    "hoursSlept": 5.1,
+    "sleepQuality": 4.6
+  }, {
+    "userID": 3,
+    "date": "2019/09/22",
+    "hoursSlept": 5.3,
+    "sleepQuality": 4.4
+  },
+];
 
 module.exports = sleepTestData;
