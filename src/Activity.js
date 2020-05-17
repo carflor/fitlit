5b68fd42a0275eef34d839fe4d0c5bce78677ab9
carflor <cfdeclet@gmail.com>
2020-05-16 18:59:57 -0600

    Build methods for user step goal and avg min active

--- src/Activity.js
+++ src/Activity.js
@@ -25,7 +25,33 @@ class Activity {
     return Number(userDistance.toFixed(2))
   }
 
+  getUserMinutesActive(allData, user, date) {
+    const filtered = allData.filter(data => data.userID === user.id)
+    const dateEntry = filtered.find(data => data.date === date)
+    return dateEntry.minutesActive
+  }
 
+  getUserAvgMinActiveByWeek(allData, user, date) {
+    const filtered = allData.filter(data => data.userID === user.id)
+    const finder = filtered.find(data => data.date === date)
+    const index = filtered.indexOf(finder)
+    const weekData = filtered.slice(index - 6, index + 1)
+    const avgMinActive = weekData.reduce((acc, data) => {
+      acc += data.minutesActive
+      return acc
+    }, 0) / weekData.length
+    return Math.floor(avgMinActive)
+  }
+
+  getUserStepGoalAchievement(allData, user, date) {
+    const filtered = allData.filter(data => data.userID === user.id)
+    const finder = filtered.find(data => data.date === date)
+    if (finder.numSteps < user.dailyStepGoal) {
+      return `Epic Fail. You did not meet your step goal!`
+    } else {
+      return `Nice job! You completed your step goal for today!`
+    }
+  }
   
 
 
