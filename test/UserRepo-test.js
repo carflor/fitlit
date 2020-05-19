71d4dfaca4c75925a27283f73472e57f49bd2600
carflor <cfdeclet@gmail.com>
2020-05-19 17:24:22 -0600

    Removed white space

--- test/UserRepo-test.js
+++ test/UserRepo-test.js
@@ -39,10 +39,42 @@ describe('UserRepo', function() {
 
   it('should get user friends names ', function() {
     expect(userRepo.getUserFriends(allUsers, user1.id)).to.deep.equal([
-      'Jarvis Considine',
-      'Herminia Witting',
-      'Mae Connelly',
-      'Erick Schaden'
+      {
+        id: 2,
+        name: 'Jarvis Considine',
+        address: '30086 Kathryn Port, Ciceroland NE 07273',
+        email: 'Dimitri.Bechtelar11@gmail.com',
+        strideLength: 4.5,
+        dailyStepGoal: 5000,
+        friends: [ 9, 18, 24, 19 ]
+      },
+      {
+        id: 3,
+        name: 'Herminia Witting',
+        address: '85823 Bosco Fork, East Oscarstad MI 85126-5660',
+        email: 'Elwin.Tromp@yahoo.com',
+        strideLength: 4.4,
+        dailyStepGoal: 5000,
+        friends: [ 19, 11, 42, 33 ]
+      },
+      {
+        id: 4,
+        name: 'Mae Connelly',
+        address: '28926 Schinner Islands, Turnermouth NE 23720-3230',
+        email: 'Marcos_Pollich@hotmail.com',
+        strideLength: 3.1,
+        dailyStepGoal: 4000,
+        friends: [ 48, 7, 44, 8 ]
+      },
+      {
+        id: 5,
+        name: 'Erick Schaden',
+        address: '514 Mayert Walk, Jordaneside SC 55023-6523',
+        email: 'Vanessa_Gerhold@gmail.com',
+        strideLength: 3.1,
+        dailyStepGoal: 8000,
+        friends: [ 13, 44, 49, 33, 10 ]
+      }
     ])
   })
 });
