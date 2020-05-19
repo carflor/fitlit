90c3f2c5a0aa51582f293cbb2d13e4a3eba28b6b
carflor <cfdeclet@gmail.com>
2020-05-19 17:23:48 -0600

    Fix test suite to reflect data changes

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -6,7 +6,7 @@ const userTestData = require('../test/user-test-data');
 const activityTestData = require('./activity-test-data')
 
 describe('Activity', function () {
-  let today, yesterday, twoDaysAgo, activityData, user, user1, activity;
+  let today, yesterday, twoDaysAgo, activityData, user, user1, activity, friendArr;
 
   beforeEach(function () {
     today = '2019/09/22'
@@ -14,6 +14,7 @@ describe('Activity', function () {
     twoDaysAgo = "2019/09/20"
     activityData = activityTestData
     activity = new Activity(activityData)
+    allUsersArr = userTestData
     user1 = userTestData[0]
     user = new User(user1)
   })
@@ -83,7 +84,7 @@ describe('Activity', function () {
 
   it('should provide user weekly data', function() {
     expect(activity.getUserWeekActivity(activityData, user, today)).to.deep.equal([
-      '2019/06/16: Steps - 6637, Minutes Active - 175, Stairs - 36',
+      '2019/09/16: Steps - 6637, Minutes Active - 175, Stairs - 36',
       '2019/09/17: Steps - 4901, Minutes Active - 288, Stairs - 10',
       '2019/09/18: Steps - 9974, Minutes Active - 80, Stairs - 40',
       '2019/09/19: Steps - 12083, Minutes Active - 218, Stairs - 20',
@@ -95,10 +96,20 @@ describe('Activity', function () {
 
   it('should provide avg steps, minutes active and flights of stairs by date for all users', function() {
     expect(activity.getAllUsersAvgData(activityData, yesterday)).to.deep.equal([ 7303, 158, 27 ])
-    expect(activity.getAllUsersAvgData(activityData, today)).to.deep.equal([ 6245, 173, 22 ])
+    expect(activity.getAllUsersAvgData(activityData, today)).to.deep.equal([ 6411, 194, 25 ])
   })
 
   it('should find the greatest stair climber amongst all users', function() {
     expect(activity.bestStairClimberEver(activityData)).to.deep.equal(45)
+  })
+  
+  it('should grab friends activity data with friends names', function() {
+    expect(activity.getFriendsStats(activityData, user, today, allUsersArr)).to.deep.equal([
+      '<br>Jarvis Considine\n      <br>Total Steps: 9050',
+      '<br>Luisa Hane\n      <br>Total Steps: 8072',
+      '<br>Erick Schaden\n      <br>Total Steps: 7073',
+      '<br>Herminia Witting\n      <br>Total Steps: 4831',
+      '<br>Mae Connelly\n      <br>Total Steps: 3030'
+    ])
   })
 });
