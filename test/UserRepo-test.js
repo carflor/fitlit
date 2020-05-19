e52afe2ea961f007e76884cb5c03253bfb245e50
carflor <cfdeclet@gmail.com>
2020-05-18 18:29:37 -0600

    Updated user data for method, created test for getUserFriends method, update results

--- test/UserRepo-test.js
+++ test/UserRepo-test.js
@@ -5,12 +5,15 @@ const User = require('../src/User');
 const userTestData = require('../test/user-test-data');
 
 describe('UserRepo', function() {
-  let user1, user2, userRepo, allUsers;
+  let user1, user2, user3, user4, user5, userRepo, allUsers;
 
   beforeEach(function() {
     user1 = userTestData[0]
     user2 = userTestData[1]
-    allUsers = [user1, user2]
+    user3 = userTestData[2]
+    user4 = userTestData[3]
+    user5 = userTestData[4]
+    allUsers = [user1, user2, user3, user4, user5]
     userRepo = new UserRepo(allUsers)
   })
 
@@ -27,10 +30,19 @@ describe('UserRepo', function() {
   })
 
   it('should select user by provided id ', function() {
-    expect(userRepo.selectedUser(2)).to.equal(user2);
+    expect(userRepo.selectedUser(user2.id)).to.equal(user2);
   })
 
   it('should calculate average step goal for all users', function() {
-    expect(userRepo.calculateAvgStepGoalUsers()).to.equal(7500);
+    expect(userRepo.calculateAvgStepGoalUsers()).to.equal(6400);
   })
-})
+
+  it('should get user friends names ', function() {
+    expect(userRepo.getUserFriends(allUsers, user1.id)).to.deep.equal([
+      'Jarvis Considine',
+      'Herminia Witting',
+      'Mae Connelly',
+      'Erick Schaden'
+    ])
+  })
+});
